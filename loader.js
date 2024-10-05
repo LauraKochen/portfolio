93212d9eead9f9b03924aaf18a926b7ee572e5b5
Laura Kochen <lmccartney3@my.bcit.ca>
2024-10-05 10:20:48 -0700

    added in portfolio images on home

--- loader.js
+++ loader.js
@@ -1,9 +1,5 @@
 window.onload = function () {
-  const overlay = document.querySelector(".overlay");
-  const content = document.querySelector(".content");
-
   setTimeout(() => {
-    overlay.style.display = "none"; // Hide the overlay
-    content.style.visibility = "visible"; // Show the content
-  }, 5000); // Adjust timing as needed (3s for spin + 2s for move)
+    document.body.style.backgroundColor = "black"; // Change to your desired background color
+  }, 3000); // Adjust time as needed
 };
---
+++ portfolio-slider.js
@@ -0,0 +1,72 @@
+const textArray = ["Designer", "Developer", "Creative Thinker"];
+let textIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textIndex].length) {
+    document.getElementById("typed-text").innerHTML +=
+      textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 150);
+  } else {
+    setTimeout(deleteText, 2000);
+  }
+}
+
+function deleteText() {
+  if (charIndex > 0) {
+    document.getElementById("typed-text").innerHTML = textArray[
+      textIndex
+    ].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(deleteText, 100);
+  } else {
+    textIndex = (textIndex + 1) % textArray.length;
+    setTimeout(type, 500);
+  }
+}
+
+type();
+
+/* type text intro */
+
+const textLines = [
+  "Hello, world!",
+  "Welcome to my website.",
+  "Enjoy your stay!",
+];
+
+let lineIndex = 0;
+let charIndex = 0;
+
+function typeLine() {
+  if (lineIndex < textLines.length) {
+    const currentLine = textLines[lineIndex];
+    const outputDiv = document.getElementById("output");
+
+    if (charIndex < currentLine.length) {
+      outputDiv.textContent += currentLine.charAt(charIndex);
+      charIndex++;
+      setTimeout(typeLine, 100); // Adjust typing speed here
+    } else {
+      outputDiv.textContent += "\n"; // Move to the next line
+      lineIndex++;
+      charIndex = 0;
+      setTimeout(typeLine, 500); // Pause before the next line
+    }
+  }
+}
+
+typeLine();
+
+// PROJECTS SLIDER
+
+document.addEventListener("DOMContentLoaded", () => {
+  const projects = document.querySelectorAll(".project");
+
+  projects.forEach((project, index) => {
+    setTimeout(() => {
+      project.classList.add("animate");
+    }, index * 500); // Adjust the timing as needed
+  });
+});
