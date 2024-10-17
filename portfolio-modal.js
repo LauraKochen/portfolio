4bb2dd335c15f77460ae16786be36d24011df841
Laura Kochen <lmccartney3@my.bcit.ca>
2024-10-17 10:30:54 -0700

    added links to nav

--- portfolio-modal.js
+++ portfolio-modal.js
@@ -1,25 +1,37 @@
 // PROJECTS MODAL CODE
 
-function openModal() {
-  document.getElementById("projectModal").style.display = "block";
+// function openModal() {
+//   document.getElementById("projectModal").style.display = "block";
+// }
+
+// function closeModal() {
+//   document.getElementById("projectModal").style.display = "none";
+// }
+
+// // Optional: Close the modal when clicking outside of it
+// window.onclick = function (event) {
+//   var modal = document.getElementById("projectModal");
+//   if (event.target === modal) {
+//     closeModal();
+//   }
+// };
+
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "block";
 }
 
-function closeModal() {
-  document.getElementById("projectModal").style.display = "none";
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "none";
 }
 
-// Optional: Close the modal when clicking outside of it
+// Close the modal when clicking outside of it
 window.onclick = function (event) {
-  var modal = document.getElementById("projectModal");
-  if (event.target === modal) {
-    closeModal();
-  }
-};
-
-// Optional: Close the modal when clicking outside of it
-window.onclick = function (event) {
-  var modal = document.getElementById("projectModal");
-  if (event.target === modal) {
-    closeModal();
-  }
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 };
